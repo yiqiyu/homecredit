3382ac03896e862d2c1ac8b45d97dfdf4687afd5
axl <axl3@tom.com>
2019-02-17 21:27:40 +0800

    cat training

--- feat_loading.py
+++ feat_loading.py
@@ -50,6 +50,297 @@ def load_extra_feats_post(app_train, app_test):
     return app_train, app_test
 
 
+def load_extra_features(app_train, app_test, *tables):
+    docs = [_f for _f in app_train.columns if 'FLAG_DOC' in _f]
+    live = [_f for _f in app_train.columns if (_f.startswith('FLAG_')) & ('FLAG_DOC' not in _f) & ('_FLAG_' not in _f)]
+
+    for df in [app_train, app_test]:
+        df['NEW_CREDIT_TO_ANNUITY_RATIO'] = df['AMT_CREDIT'] / df['AMT_ANNUITY']
+        df['NEW_EXT_SOURCES_MEAN'] = df[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].mean(axis=1)
+        df['NEW_CREDIT_TO_GOODS_RATIO'] = df['AMT_CREDIT'] / df['AMT_GOODS_PRICE']
+        df['NEW_CAR_TO_BIRTH_RATIO'] = df['OWN_CAR_AGE'] / df['DAYS_BIRTH']
+        df['NEW_CAR_TO_EMPLOY_RATIO'] = df['OWN_CAR_AGE'] / df['DAYS_EMPLOYED']
+        df['NEW_PHONE_TO_BIRTH_RATIO'] = df['DAYS_LAST_PHONE_CHANGE'] / df['DAYS_BIRTH']
+        df['NEW_PHONE_TO_EMPLOY_RATIO'] = df['DAYS_LAST_PHONE_CHANGE'] / df['DAYS_EMPLOYED']
+        df['NEW_CREDIT_TO_INCOME_RATIO'] = df['AMT_CREDIT'] / df['AMT_INCOME_TOTAL']
+        df['WORKING_LIFE_RATIO'] = df['DAYS_EMPLOYED'] / df['DAYS_BIRTH']
+        df['INCOME_PER_FAM'] = df['AMT_INCOME_TOTAL'] / df['CNT_FAM_MEMBERS']
+        df['CHILDREN_RATIO'] = df['CNT_CHILDREN'] / df['CNT_FAM_MEMBERS']
+        df['document_sum'] = df['FLAG_DOCUMENT_2'] + df['FLAG_DOCUMENT_3'] + df['FLAG_DOCUMENT_4'] + df[
+            'FLAG_DOCUMENT_5'] + df['FLAG_DOCUMENT_6'] + df['FLAG_DOCUMENT_7'] + df['FLAG_DOCUMENT_8'] + df[
+                                  'FLAG_DOCUMENT_9'] + df['FLAG_DOCUMENT_10'] + df['FLAG_DOCUMENT_11'] + df[
+                                  'FLAG_DOCUMENT_12'] + df['FLAG_DOCUMENT_13'] + df['FLAG_DOCUMENT_14'] + df[
+                                  'FLAG_DOCUMENT_15'] + df['FLAG_DOCUMENT_16'] + df['FLAG_DOCUMENT_17'] + df[
+                                  'FLAG_DOCUMENT_18'] + df['FLAG_DOCUMENT_19'] + df['FLAG_DOCUMENT_20'] + df[
+                                  'FLAG_DOCUMENT_21']
+        df['credit_minus_goods'] = df['AMT_CREDIT'] - df['AMT_GOODS_PRICE']
+        df['reg_div_publish'] = df['DAYS_REGISTRATION'] / df['DAYS_ID_PUBLISH']
+        df['birth_div_reg'] = df['DAYS_BIRTH'] / df['DAYS_REGISTRATION']
+        df['ANN_LENGTH_EMPLOYED_RATIO'] = (df['AMT_CREDIT'] / df['AMT_ANNUITY']) / df['DAYS_EMPLOYED']
+        df['age_finish'] = df['DAYS_BIRTH'] * (-1.0 / 365) + (df['AMT_CREDIT'] / df['AMT_ANNUITY']) * (
+                1.0 / 12)  # how old when finish
+        df['ANN_LENGTH_AGE_RATIO'] = (df['AMT_CREDIT'] / df['AMT_ANNUITY']) * (1.0 / 12) / df['DAYS_BIRTH'] * (
+                -1.0 / 365)
+        df['NEW_DOC_IND_KURT'] = df[docs].kurtosis(axis=1)
+        df['NEW_LIVE_IND_SUM'] = df[live].sum(axis=1)
+
+        df["OBS_30_60_diff"] = (df["OBS_30_CNT_SOCIAL_CIRCLE"] - df["OBS_60_CNT_SOCIAL_CIRCLE"])
+
+    for table in tables:
+        app_train = app_train.merge(right=table.reset_index(), how='left', on='SK_ID_CURR')
+        app_test = app_test.merge(right=table.reset_index(), how='left', on='SK_ID_CURR')
+    return app_train, app_test
+
+
+def load_bureau(bureau, buro_balance):
+    bb_aggregations = {'MONTHS_BALANCE': ['min', 'max', 'size']}
+    bb_cat = [col for col in buro_balance.columns if len(buro_balance[col].unique()) == 2]
+    bb_aggregations.update({col: ["mean"] for col in bb_cat})
+    buro_balance_agg = buro_balance.groupby('SK_ID_BUREAU').agg(bb_aggregations)
+    buro_balance_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in buro_balance_agg.columns.tolist()])
+    bureau = bureau.join(buro_balance_agg, how='left', on='SK_ID_BUREAU')
+    bureau.drop(['SK_ID_BUREAU'], axis=1, inplace=True)
+    del buro_balance_agg
+    gc.collect()
+
+    bureau["DAYS_CREDIT_ITERVAL"] = bureau["DAYS_CREDIT"] - bureau["DAYS_CREDIT_ENDDATE"]
+    bureau["CREDIT_SUM_DEBT_RATIO"] = bureau["AMT_CREDIT_SUM_DEBT"] / bureau["AMT_CREDIT_SUM"]
+    bureau["CREDIT_SUM_OVERDUE_RATIO"] = bureau["AMT_CREDIT_SUM_OVERDUE"] / bureau["AMT_CREDIT_SUM"]
+    bureau["CREDIT_SUM_LIMIT_DEBT_RATIO"] = bureau["AMT_CREDIT_SUM_DEBT"] / bureau["AMT_CREDIT_SUM_LIMIT"]
+    bureau["CREDIT_SUM_LIMIT_OVERDUE_RATIO"] = bureau["AMT_CREDIT_SUM_OVERDUE"] / bureau["AMT_CREDIT_SUM_LIMIT"]
+    bureau["ANNUITY_DEBT_RATIO"] = bureau["AMT_CREDIT_SUM_DEBT"] / bureau["AMT_ANNUITY"]
+    bureau["ANNUITY_OVERDUE_RATIO"] = bureau["AMT_CREDIT_SUM_OVERDUE"] / bureau["AMT_ANNUITY"]
+    bureau["MAX_OVERDUE_RATIO"] = bureau["AMT_CREDIT_SUM_OVERDUE"] / bureau["AMT_CREDIT_MAX_OVERDUE"]
+    num_aggregations = {
+        'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
+        'DAYS_CREDIT_ENDDATE': ['min', 'max', 'mean'],
+        'DAYS_CREDIT_UPDATE': ['mean'],
+        'CREDIT_DAY_OVERDUE': ['max', 'mean'],
+        'AMT_CREDIT_MAX_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM': ['max', 'mean', 'sum'],
+        'AMT_CREDIT_SUM_DEBT': ['max', 'mean', 'sum'],
+        'AMT_CREDIT_SUM_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
+        'AMT_ANNUITY': ['max', 'mean'],
+        'CNT_CREDIT_PROLONG': ['sum'],
+        'MONTHS_BALANCE_MIN': ['min'],
+        'MONTHS_BALANCE_MAX': ['max'],
+        'MONTHS_BALANCE_SIZE': ['mean', 'sum'],
+
+        "DAYS_CREDIT_ITERVAL": ['min', 'max', 'mean', 'var'],
+        "CREDIT_SUM_DEBT_RATIO": ['min', 'max', 'mean', 'var'],
+        "CREDIT_SUM_OVERDUE_RATIO": ['min', 'max', 'mean', 'var'],
+        "CREDIT_SUM_LIMIT_DEBT_RATIO": ['min', 'max', 'mean', 'var'],
+        "CREDIT_SUM_LIMIT_OVERDUE_RATIO": ['min', 'max', 'mean', 'var'],
+        "ANNUITY_DEBT_RATIO": ['min', 'max', 'mean', 'var'],
+        "ANNUITY_OVERDUE_RATIO": ['min', 'max', 'mean', 'var'],
+        "MAX_OVERDUE_RATIO": ['min', 'max', 'mean', 'var'],
+    }
+    cat_aggregations = {col: ["mean"] for col in bureau.columns if len(bureau[col].unique()) == 2}
+    bureau_agg = bureau.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    bureau_agg.columns = pd.Index(['BURO_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
+
+    active = bureau[bureau['CREDIT_ACTIVE_Active'] == 1]
+    active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
+    active_agg.columns = pd.Index(['ACTIVE_' + e[0] + "_" + e[1].upper() for e in active_agg.columns.tolist()])
+    bureau_agg = bureau_agg.join(active_agg, how='left', on='SK_ID_CURR')
+    del active, active_agg
+    gc.collect()
+    # Bureau: Closed credits - using only numerical aggregations
+    closed = bureau[bureau['CREDIT_ACTIVE_Closed'] == 1]
+    closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
+    closed_agg.columns = pd.Index(['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
+    bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
+    del closed, closed_agg
+    gc.collect()
+    return bureau_agg
+
+
+def load_prev(prev):
+    prev['DAYS_FIRST_DRAWING'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_FIRST_DUE'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_LAST_DUE_1ST_VERSION'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_LAST_DUE'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_TERMINATION'].replace(365243, np.nan, inplace=True)
+
+    prev['APP_CREDIT_PERC'] = prev['AMT_APPLICATION'] / prev['AMT_CREDIT']
+    prev["APP_DURATION"] = prev["DAYS_TERMINATION"] - prev["DAYS_DECISION"]
+    prev["APP_PAY_DURATION"] = prev["DAYS_LAST_DUE"] - prev["DAYS_FIRST_DRAWING"]
+    prev["INTREST_RATE_RATIO"] = prev["RATE_INTEREST_PRIVILEGED"] / prev["RATE_INTEREST_PRIMARY"]
+    prev['CREDIT_TO_GOODS_RATIO'] = prev['AMT_CREDIT'] / prev['AMT_GOODS_PRICE']
+
+    # Previous applications numeric features
+    num_aggregations = {
+        'AMT_ANNUITY': ['min', 'max', 'mean'],
+        'AMT_APPLICATION': ['min', 'max', 'mean'],
+        'AMT_CREDIT': ['min', 'max', 'mean'],
+        'APP_CREDIT_PERC': ['min', 'max', 'mean', 'var'],
+        'AMT_DOWN_PAYMENT': ['min', 'max', 'mean'],
+        'AMT_GOODS_PRICE': ['min', 'max', 'mean'],
+        'HOUR_APPR_PROCESS_START': ['min', 'max', 'mean'],
+        'RATE_DOWN_PAYMENT': ['min', 'max', 'mean'],
+        'DAYS_DECISION': ['min', 'max', 'mean'],
+        'CNT_PAYMENT': ['mean', 'sum'],
+
+        "APP_DURATION": ['max', 'mean', 'sum'],
+        "APP_PAY_DURATION": ['max', 'mean', 'sum'],
+        "INTREST_RATE_RATIO": ['max', 'mean', 'sum', 'var'],
+        'CREDIT_TO_GOODS_RATIO': ['max', 'mean', 'sum', 'var'],
+    }
+    cat_aggregations = {col: ["mean"] for col in prev.columns if len(prev[col].unique()) == 2}
+    prev_agg = prev.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    prev_agg.columns = pd.Index(['PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns.tolist()])
+    #     print(prev_agg.columns.tolist())
+    #     prev_agg = prev_agg.join(prev_agg, how='left', on='SK_ID_CURR')
+
+    approved = prev[prev['NAME_CONTRACT_STATUS_Approved'] == 1]
+    approved_agg = approved.groupby('SK_ID_CURR').agg(num_aggregations)
+    approved_agg.columns = pd.Index(['APPROVED_' + e[0] + "_" + e[1].upper() for e in approved_agg.columns.tolist()])
+    prev_agg = prev_agg.join(approved_agg, how='left', on='SK_ID_CURR')
+    del approved, approved_agg
+    # Previous Applications: Refused Applications - only numerical features
+    refused = prev[prev['NAME_CONTRACT_STATUS_Refused'] == 1]
+    refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
+    refused_agg.columns = pd.Index(['REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns.tolist()])
+    prev_agg = prev_agg.join(refused_agg, how='left', on='SK_ID_CURR')
+    del refused, refused_agg
+    gc.collect()
+    return prev_agg
+
+
+def load_install(inst):
+    inst["DPD"] = inst['DAYS_ENTRY_PAYMENT'] - inst['DAYS_INSTALMENT']
+    inst["DBD"] = inst['DAYS_INSTALMENT'] - inst['DAYS_ENTRY_PAYMENT']
+    inst['DPD'] = inst['DPD'].apply(lambda x: x if x > 0 else 0)
+    inst['DBD'] = inst['DBD'].apply(lambda x: x if x > 0 else 0)
+    inst['PAYMENT_PERC'] = inst['AMT_PAYMENT'] / inst['AMT_INSTALMENT']
+    inst['PAYMENT_DIFF'] = inst['AMT_INSTALMENT'] - inst['AMT_PAYMENT']
+
+    aggregations = {
+        'NUM_INSTALMENT_VERSION': ['nunique'],
+        'DPD': ['max', 'mean', 'sum'],
+        'DBD': ['max', 'mean', 'sum'],
+        'PAYMENT_PERC': ['max', 'mean', 'sum', 'var'],
+        'PAYMENT_DIFF': ['max', 'mean', 'sum', 'var'],
+        'AMT_INSTALMENT': ['max', 'mean', 'sum'],
+        'AMT_PAYMENT': ['min', 'max', 'mean', 'sum'],
+    }
+    aggregations.update({col: ["mean"] for col in inst.columns if len(inst[col].unique()) == 2})
+    inst_agg = inst.groupby('SK_ID_CURR').agg(aggregations)
+    #     print(inst_agg.columns)
+    inst_agg.columns = pd.Index(['INSTAL_' + e[0] + "_" + e[1].upper() for e in inst_agg.columns.tolist()])
+    inst_agg["INSTAL_TOTAL_PAYMENT_RATE"] = inst_agg["INSTAL_AMT_PAYMENT_SUM"] / inst_agg["INSTAL_AMT_INSTALMENT_SUM"]
+    inst_agg['INSTAL_COUNT'] = inst.groupby('SK_ID_CURR').size()
+    #     del inst
+    #     gc.collect()
+    return inst_agg
+
+
+def load_cash(cash):
+    cash["INSTALMENT_FUTURE_RATIO"] = cash["CNT_INSTALMENT_FUTURE"] / cash["CNT_INSTALMENT"]
+    cash["INSTALMENT_BALANCE_RATIO"] = cash["CNT_INSTALMENT_FUTURE"] / cash["MONTHS_BALANCE"].abs()
+    aggregations = {
+        'MONTHS_BALANCE': ['max', 'mean', 'size'],
+        'SK_DPD': ['max', 'mean'],
+        'SK_DPD_DEF': ['max', 'mean'],
+        "CNT_INSTALMENT": ['max', 'mean', 'sum', 'var'],
+        "CNT_INSTALMENT_FUTURE": ['max', 'mean', 'sum', 'var'],
+
+        "INSTALMENT_FUTURE_RATIO": ['max', 'mean', 'sum', 'var'],
+        "INSTALMENT_BALANCE_RATIO": ['max', 'mean', 'sum', 'var'],
+    }
+    aggregations.update({col: ["mean"] for col in cash.columns if len(cash[col].unique()) == 2})
+    cash_agg = cash.groupby('SK_ID_CURR').agg(aggregations)
+    cash_agg.columns = pd.Index(['POS_' + e[0] + "_" + e[1].upper() for e in cash_agg.columns.tolist()])
+    # Count pos cash accounts
+    cash_agg['POS_COUNT'] = cash.groupby('SK_ID_CURR').size()
+    return cash_agg
+
+
+def load_credit(credit):
+    credit["INSTALMENT_MATURE_BALANCE_RATIO"] = credit["CNT_INSTALMENT_MATURE_CUM"] / credit["MONTHS_BALANCE"].abs()
+    credit["POS_AMT_MEAN"] = credit["AMT_DRAWINGS_POS_CURRENT"] / credit["CNT_DRAWINGS_POS_CURRENT"]
+    credit["PAYMENT_DIFF"] = credit["AMT_PAYMENT_CURRENT"] - credit["AMT_PAYMENT_TOTAL_CURRENT"]
+    credit["RECEIVABLE_DIFF"] = credit["AMT_TOTAL_RECEIVABLE"] - credit["AMT_RECIVABLE"]
+    credit["RECEIVABLE_PRINCIPAL_DIFF"] = credit["AMT_TOTAL_RECEIVABLE"] - credit["AMT_RECEIVABLE_PRINCIPAL"]
+
+    for col in ["CNT_DRAWINGS_ATM_CURRENT", "CNT_DRAWINGS_OTHER_CURRENT", "CNT_DRAWINGS_POS_CURRENT"]:
+        credit["RELATIVE" + col[col.find("_"):]] = credit[col] / credit["CNT_DRAWINGS_CURRENT"]
+    aggregations = {
+        'MONTHS_BALANCE': ['max', 'mean', "min", 'size'],
+        'AMT_BALANCE': ['max', 'mean', 'sum', 'var'],
+        'AMT_CREDIT_LIMIT_ACTUAL': ['max', 'mean', 'sum', 'var'],
+        "AMT_DRAWINGS_ATM_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "AMT_DRAWINGS_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "AMT_DRAWINGS_OTHER_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "AMT_DRAWINGS_POS_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "AMT_INST_MIN_REGULARITY": ['max', 'mean', 'sum', 'var'],
+        "AMT_PAYMENT_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "AMT_PAYMENT_TOTAL_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "AMT_RECEIVABLE_PRINCIPAL": ['max', 'mean', 'sum', 'var'],
+        "AMT_RECIVABLE": ['max', 'mean', 'sum', 'var'],
+        "AMT_TOTAL_RECEIVABLE": ['max', 'mean', 'sum', 'var'],
+        "CNT_DRAWINGS_ATM_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "CNT_DRAWINGS_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "CNT_DRAWINGS_OTHER_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "CNT_DRAWINGS_POS_CURRENT": ['max', 'mean', 'sum', 'var'],
+        "CNT_INSTALMENT_MATURE_CUM": ['max', 'mean', 'sum', 'var'],
+        'SK_DPD': ['max', 'mean'],
+        'SK_DPD_DEF': ['max', 'mean'],
+
+        "INSTALMENT_MATURE_BALANCE_RATIO": ['max', 'mean', 'sum', 'var'],
+        "POS_AMT_MEAN": ['max', 'mean', 'sum', 'var'],
+        "PAYMENT_DIFF": ['max', 'mean', 'sum', 'var'],
+        "RECEIVABLE_DIFF": ['max', 'mean', 'sum', 'var'],
+        "RECEIVABLE_PRINCIPAL_DIFF": ['max', 'mean', 'sum', 'var'],
+    }
+    aggregations.update({"RELATIVE" + col[col.find("_"):]: ['max', 'mean', 'sum', 'var'] for col in
+                         ["CNT_DRAWINGS_ATM_CURRENT", "CNT_DRAWINGS_OTHER_CURRENT", "CNT_DRAWINGS_POS_CURRENT"]})
+    aggregations.update({col: ["mean"] for col in credit.columns if len(credit[col].unique()) == 2})
+    cc_agg = credit.groupby('SK_ID_CURR').agg(aggregations)
+    cc_agg.columns = pd.Index(['CC_' + e[0] + "_" + e[1].upper() for e in cc_agg.columns.tolist()])
+    # Count credit card lines
+    cc_agg['CC_COUNT'] = credit.groupby('SK_ID_CURR').size()
+    return cc_agg
+
+
+def load_all_tables(train="app_train_new.csv", test="app_test_new.csv"):
+    to_del_cols = ['APARTMENTS_AVG', 'BASEMENTAREA_AVG', 'YEARS_BEGINEXPLUATATION_AVG', 'YEARS_BUILD_AVG',
+                   'COMMONAREA_AVG', 'ELEVATORS_AVG', 'ENTRANCES_AVG', 'FLOORSMAX_AVG', 'FLOORSMIN_AVG', 'LANDAREA_AVG',
+                   'LIVINGAPARTMENTS_AVG', 'LIVINGAREA_AVG', 'NONLIVINGAPARTMENTS_MEDI', 'NONLIVINGAREA_MEDI',
+                   'AMT_GOODS_PRICE', 'APARTMENTS_MEDI', 'BASEMENTAREA_MEDI', 'YEARS_BUILD_MEDI', 'COMMONAREA_MODE',
+                   'ELEVATORS_MODE', 'ENTRANCES_MEDI', 'FLOORSMAX_MEDI', 'FLOORSMIN_MODE', 'LANDAREA_MODE',
+                   'LIVINGAPARTMENTS_MEDI', 'LIVINGAREA_MODE', 'OBS_30_CNT_SOCIAL_CIRCLE']
+    app_train = load_dataframe(train)
+    app_test = load_dataframe(test)
+    app_train, app_test = load_extra_features(app_train, app_test)
+    # app_train = app_train.drop(to_del_cols, axis=1)
+    # app_test = app_test.drop(to_del_cols, axis=1)
+
+
+    bureau = load_dataframe("bureau_new.csv")
+    buro_balance = load_dataframe("bureau_balance_new.csv")
+    bbb = load_bureau(bureau, buro_balance)
+
+    app_train = app_train.merge(right=bbb.reset_index(), how='left', on='SK_ID_CURR')
+    app_test = app_test.merge(right=bbb.reset_index(), how='left', on='SK_ID_CURR')
+    del bureau, buro_balance
+    gc.collect()
+
+    for name, load_method in {"cash": load_cash, "credit": load_credit, "installments": load_install,
+                              "previous": load_prev}.items():
+        df = load_method(load_dataframe(name + "_new.csv"))
+        app_train = app_train.merge(right=df.reset_index(), how='left', on='SK_ID_CURR')
+        app_test = app_test.merge(right=df.reset_index(), how='left', on='SK_ID_CURR')
+        del df
+        gc.collect()
+
+    to_del = del_single_variance_and_nan_too_much(app_train)
+    app_test = app_test.drop(to_del, axis=1)
+
+    return app_train, app_test
+
+
 def del_single_variance_and_nan_too_much(ds):
     total_rows = ds.shape[0]
     null_count = 0
--- stacking.py
+++ stacking.py
@@ -14,7 +14,7 @@ import gc
 import copy
 import pandas as pd
 
-from feat_loading import load_dataframe, load_extra_feats_post, del_single_variance_and_nan_too_much
+from feat_loading import *
 
 
 class StackingWrapper(object):
@@ -29,7 +29,7 @@ class StackingWrapper(object):
         return getattr(self.clf, item)
 
 
-def get_oof(clf_proto, train, test, target_name, n_folds=5):
+def get_oof(clf_proto, train, test, target_name, n_folds=5, **fit_params):
     train_ids = train['SK_ID_CURR']
     test_ids = test['SK_ID_CURR']
     test = test.drop(['SK_ID_CURR'], axis=1)
@@ -45,7 +45,7 @@ def get_oof(clf_proto, train, test, target_name, n_folds=5):
         tnX, tny = X.loc[train_indices, :], y[train_indices]
         pX = X.loc[predict_indices, :]
         clf = copy.deepcopy(clf_proto)
-        clf.fit(tnX, tny)
+        clf.fit(tnX, tny, **fit_params)
         train_oof[predict_indices] = clf.predict_proba(pX)[:, 1]
         test_predictions += clf.predict_proba(test)[:, 1] / k_fold.n_splits
 
@@ -63,21 +63,22 @@ def get_oof(clf_proto, train, test, target_name, n_folds=5):
 
 if __name__ == '__main__':
     print("start loading")
-    app_train = load_dataframe("train_all.csv")
-    app_test = load_dataframe("test_all.csv")
-    app_train, app_test = load_extra_feats_post(app_train, app_test)
-    # app_train = Imputer().fit_transform(app_train)
-    # app_test = Imputer().fit_transform(app_test)
-    # for dsname, ds in {"app_train": app_train, "app_test":app_test}.items():
-    #     for col in ds:
-    #         if ds[np.isinf(ds[col])].any():
-    #             ds[col] = ds[col].replace(np.inf, 0)
-    to_del = del_single_variance_and_nan_too_much(app_train)
-    app_test = app_test.drop(to_del, axis=1)
-    app_train = app_train.fillna(999999)
-    app_test = app_test.fillna(999999)
-    app_train = app_train.replace([np.inf, -np.inf], [99999, -99999])
-    app_test = app_test.replace([np.inf, -np.inf], [99999, -99999])
+    # app_train = load_dataframe("train_all.csv")
+    # app_test = load_dataframe("test_all.csv")
+    # app_train, app_test = load_extra_feats_post(app_train, app_test)
+    # to_del = del_single_variance_and_nan_too_much(app_train)
+    # app_test = app_test.drop(to_del, axis=1)
+    # app_train = app_train.fillna(999999)
+    # app_test = app_test.fillna(999999)
+    # app_train = app_train.replace([np.inf, -np.inf], [99999, -99999])
+    # app_test = app_test.replace([np.inf, -np.inf], [99999, -99999])
+
+    # -----------------------------cat load-----------------------------------
+    # app_train, app_test = load_all_tables("application_train.csv", "application_test.csv")
+    # app_train.to_csv("train_all_cat.csv", index=False)
+    # app_test.to_csv("test_all_cat.csv", index=False)
+    app_train = load_dataframe("train_all_cat.csv")
+    app_test = load_dataframe("test_all_cat.csv")
 
     # -------------------------------------Extra Tree--------------------------------------------
     # print("start training random forest")
@@ -100,30 +101,39 @@ if __name__ == '__main__':
     # rf_test_feat.columns = ["SK_ID_CURR","TARGET"]
     # rf_test_feat.to_csv("rf_submission.csv", index=False)
 
-    # -------------------------------------Extra Tree--------------------------------------------
-    print("start training extra tree")
-    et_settings = dict(
-        n_estimators=1500,
-        max_depth=10,
-        min_samples_leaf=20,
-        min_samples_split=10,
-        min_impurity_decrease=1e-5,
-        n_jobs=5,
-        random_state=50,
-        verbose=1
-    )
-    et = ExtraTreesClassifier(**et_settings)
-    et_train_oof, et_test_feat = get_oof(et, app_train, app_test, "ET")
-    print("recording")
-    et_train_oof.to_csv("ET_oof.csv")
-    et_test_feat.to_csv("ET_test.csv")
+    # # -------------------------------------Extra Tree--------------------------------------------
+    # print("start training extra tree")
+    # et_settings = dict(
+    #     n_estimators=1200,
+    #     max_depth=10,
+    #     min_samples_leaf=20,
+    #     min_samples_split=10,
+    #     min_impurity_decrease=1e-6,
+    #     n_jobs=5,
+    #     random_state=50,
+    #     verbose=1
+    # )
+    # et = ExtraTreesClassifier(**et_settings)
+    # et_train_oof, et_test_feat = get_oof(et, app_train, app_test, "ET")
+    # print("recording")
+    # et_train_oof.to_csv("ET_oof.csv")
+    # et_test_feat.to_csv("ET_test.csv")
+    #
+    # et_test_feat.columns = ["SK_ID_CURR","TARGET"]
+    # et_test_feat.to_csv("et_submission.csv", index=False)
 
-    et_test_feat.columns = ["SK_ID_CURR","TARGET"]
-    et_test_feat.to_csv("et_submission.csv", index=False)
+    # -------------------------------------Catboost--------------------------------------------
 
+    print("start catboost")
+    cat_feats = np.where(app_train.dtypes == np.object)[0]
+    app_train.loc[cat_feats, :].fillna("NaN", inplace=True)
+    app_test.loc[cat_feats, :].fillna("NaN", inplace=True)
+    app_train = app_train.fillna(999999)
+    app_test = app_test.fillna(999999)
+    cat_feats = cat_feats-2
     cb_params = {
-        'iterations':9000,
-        'learning_rate':0.05,
+        'iterations':200,
+        'learning_rate':0.1,
         'depth':10,
         'l2_leaf_reg':40,
         'bootstrap_type':'Bernoulli',
@@ -133,5 +143,18 @@ if __name__ == '__main__':
         'metric_period':50,
         'od_type':'Iter',
         'od_wait':45,
-        'allow_writing_files':False
+        'allow_writing_files':False,
+        # "early_stopping_rounds": 300,
+        "verbose": 200,
+        "thread_count": 5,
+
         }
+
+    cb = CatBoostClassifier(**cb_params)
+    cb_train_oof, cb_test_feat = get_oof(cb, app_train, app_test, "CB", cat_features=cat_feats)
+    print("recording")
+    cb_train_oof.to_csv("CB_oof.csv")
+    cb_test_feat.to_csv("CB_test.csv")
+
+    cb_test_feat.columns = ["SK_ID_CURR","TARGET"]
+    cb_test_feat.to_csv("cb_submission.csv", index=False)
