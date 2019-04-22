3a7f76f5687dda8995ba3015d011eaded1d18b2e
yyq <dante3@126.com>
2019-04-22 17:59:32 +0800

    sparksession

--- cluster_train.py
+++ cluster_train.py
@@ -1,10 +1,29 @@
 from mmlspark import LightGBMRegressor
 from mmlspark import LightGBMRegressionModel
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+
+from feat_loading import *
+
+app_train = load_dataframe("train_all2.csv")
+app_test = load_dataframe("test_all2.csv")
+app_train, app_test = load_extra_feats_post(app_train, app_test)
+to_del = del_useless_cols(app_train)
+app_test = app_test.drop(to_del, axis=1)
 
 
 conf = SparkConf().setMaster("spark://master:7077").setAppName("MMLSPARK")
 sc = SparkContext(conf = conf)
+
+spark = SparkSession \
+        .builder \
+        .appName("Python Spark SQL Hive integration example") \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+app_train_spark = spark.createDataFrame(app_train)
+app_test_spark = spark.createDataFrame(app_test)
+
 
 model = LightGBMRegressor(boosting="goss", numIterations=10000, objective='binary',
         learningRate=0.005, seed=50,
