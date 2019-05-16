b4fe47b349f9f875f0ed4b8d37a9f4fc7e9566be
axl <axl3@tom.com>
2019-05-16 23:36:27 +0800

    fixing

--- cluster_train.py
+++ cluster_train.py
@@ -4,7 +4,6 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 import os
 
-os.getcwd()
 
 conf = SparkConf().setMaster("spark://master:7077").setAppName("MMLSPARK")
 sc = SparkContext(conf = conf)
@@ -12,28 +11,33 @@ sc = SparkContext(conf = conf)
 # spark-submit --py-files /mnt/d/data_analysis/homecredit2/feat_loading.py
 from feat_loading import *
 
-app_train = load_dataframe("/mnt/d/data_analysis/homecredit2/train_all2.csv")
-app_test = load_dataframe("/mnt/d/data_analysis/homecredit2/test_all2.csv")
-app_train, app_test = load_extra_feats_post(app_train, app_test)
-to_del = del_useless_cols(app_train)
-app_test = app_test.drop(to_del, axis=1)
+# app_train = load_dataframe("/mnt/d/data_analysis/homecredit2/train_all2.csv")
+# app_test = load_dataframe("/mnt/d/data_analysis/homecredit2/test_all2.csv")
+# app_train, app_test = load_extra_feats_post(app_train, app_test)
+# to_del = del_useless_cols(app_train)
+# app_test = app_test.drop(to_del, axis=1)
 
 
 spark = SparkSession \
         .builder \
-        .appName("Python Spark SQL Hive integration example") \
+        .appName("MMLSPARK") \
         .enableHiveSupport() \
         .getOrCreate()
 
-app_train_spark = spark.createDataFrame(app_train)
-app_test_spark = spark.createDataFrame(app_test)
+# app_train_spark = spark.createDataFrame(app_train).collect()
+# app_test_spark = spark.createDataFrame(app_test).collect()
 
 
-model = LightGBMRegressor(boosting="goss", numIterations=10000, objective='binary',
-        learningRate=0.005, seed=50,
-        baggingFraction=0.87, lambdaL1=0.4, lambdaL2=0.4, minDataInLeaf=30,
+app_train = spark.read.csv("/homeredit/train_all2.csv", header='true', inferSchema='true')
+app_test = spark.read.csv("/homeredit/test_all2.csv", header='true', inferSchema='true')
+
+
+model = LightGBMRegressor(boostingType="goss", numIterations=10000, objective='binary',
+        learningRate=0.005, baggingSeed=50,
+        baggingFraction=0.87, lambdaL1=0.4, lambdaL2=0.4, minSumHessianInLeaf=0.003,
         maxDepth=9, featureFraction=0.66, numLeaves=47)
 
+model.fit(app_train)
 
 model.saveNativeModel("mymodel")
 model = LightGBMRegressionModel.loadNativeModelFromFile("mymodel")
