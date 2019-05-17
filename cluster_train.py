c7d58fadf0ff5b6ed8532b54ac189bc0f066d3bf
yyq <dante3@126.com>
2019-05-17 11:52:14 +0800

    add standard procedure

--- cluster_train.py
+++ cluster_train.py
@@ -2,6 +2,10 @@ from mmlspark import LightGBMRegressor
 from mmlspark import LightGBMRegressionModel
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
+import pyspark.ml.feature as ft
+from pyspark.ml import Pipeline
+import pyspark.ml.evaluation as ev
+
 import os
 
 
@@ -32,12 +36,42 @@ app_train = spark.read.csv("/homeredit/train_all2.csv", header='true', inferSche
 app_test = spark.read.csv("/homeredit/test_all2.csv", header='true', inferSchema='true')
 
 
-model = LightGBMRegressor(boostingType="goss", numIterations=10000, objective='binary',
+featuresCreator = ft.VectorAssembler(
+    inputCols=app_train.columns,
+    outputCol='features'
+    )
+
+
+lgbm = LightGBMRegressor(boostingType="goss", numIterations=10000, objective='binary',
         learningRate=0.005, baggingSeed=50,
         baggingFraction=0.87, lambdaL1=0.4, lambdaL2=0.4, minSumHessianInLeaf=0.003,
-        maxDepth=9, featureFraction=0.66, numLeaves=47)
+        maxDepth=9, featureFraction=0.66, numLeaves=47,
+        labelCol="TARGET"
+                          )
 
-model.fit(app_train)
 
-model.saveNativeModel("mymodel")
-model = LightGBMRegressionModel.loadNativeModelFromFile("mymodel")
+pipeline = Pipeline(stages=[
+                # 特征整理
+                featuresCreator,
+                # 模型名称
+                    lgbm])
+
+tr, te = app_train.randomSplit([0.7, 0.3], seed=666)
+
+vmodel = pipeline.fit(tr)
+t_model = vmodel.transform(te)
+
+
+evaluator = ev.BinaryClassificationEvaluator(
+    rawPredictionCol='probability',
+    labelCol='TARGET')
+print(evaluator.evaluate(t_model,
+{evaluator.metricName: 'areaUnderROC'}))
+
+
+# model = pipeline.fit(app_train)
+# res = model.transform(app_test)
+
+
+# model.saveNativeModel("mymodel")
+# model = LightGBMRegressionModel.loadNativeModelFromFile("mymodel")
